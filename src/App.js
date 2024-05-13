03932e2bcd8ac02685dd37c25211e0747993f5ab
spxerek <sarveshpingulkar1@gmail.com>
2024-05-13 17:26:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import ParticlesComponent from './components/particle';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesComponent id= "particles"/>
+      <p className='nre'>Spxerek</p>
+      <p className='nre1'>F</p>
     </div>
+    
   );
 }
 
